aa17867157ed822534d0f7c2bf1e97ce68ed8ff6
laiminjie <10452545@qq.com>
2022-05-14 19:07:06 +0800

    mast commit

--- src/main/java/com/mos/git/GitTest.java
+++ src/main/java/com/mos/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("3");
         System.out.println("4");
         System.out.println("5");
+        System.out.println("7");
     }
 
     private void aa(){
