45dc88612c0d8dd687b6171b93801633d6f7aaa5
laiminjie <10452545@qq.com>
2022-05-14 18:46:45 +0800

    three commit

--- src/main/java/com/mos/git/GitTest.java
+++ src/main/java/com/mos/git/GitTest.java
@@ -2,7 +2,8 @@ package com.mos.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("123123");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
